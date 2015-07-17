e2711cd4dd267a48c46e8f8f26b148ee882abb17
zvbsm <tdelgabsm@gmail.com>
2015-07-16 21:38:09 -0700

    to-do list app
    My first personal project

---
+++ js/todolist.js
@@ -0,0 +1,105 @@
+$(function() {
+
+	var $colors = $('#selectColor');
+	//text typed in by the user
+	var $textInput = $('input:text');
+	//for changing placeholder value
+	var $inputLabel = $('#inputLabel');
+	//the color the user selects in the color selection div to apply to
+	//the item they are creating
+	var thisColor = 'white';
+	var colorWheel = ['red', 'yellow', 'green', 'white'];
+	//0/1 = off/on
+	var deleteMode = 0;
+
+	//hide the color selector until user focuses on the text input
+	//to create a new item
+	$colors.hide();	
+
+	//when user focuses on the text box to start adding a new label, show
+	//the color selection div
+	$inputLabel.on('focus', function() {
+		$colors.show();
+	});
+
+	//get the value of whichever button the user selects to apply to the 
+	//new item
+	$('button[class="color"]').on('click', function() {
+		thisColor = this.id;
+		$('#thisColorText').remove();
+		$('#colorText').after('<p id="thisColorText">' + thisColor + '</p>');
+	});
+
+	//when user submits the form, get the user input and create a new list
+	//item, add it to the start of the list and update its class attribute to
+	//whatever the user selected. if the user doesnt select any class, it will
+	//show default white color
+	$('#controls').on('submit', function(e) {
+		e.preventDefault();
+		var newText = $textInput.val();
+		if(newText === '') {
+			$inputLabel.attr('placeholder', 'No text entered!');
+			return;
+		};
+		$('li:first').before('<li>' + newText + '</li>');
+		$('li:first').addClass(thisColor);
+		$('li:first').attr('id', 'li' + $('li').length);
+//		.bind('selectstart', function(){ return false; });
+		//empty the content of variables used in this session for new items
+		//and hide the color selection div
+		$textInput.val('');
+		thisColor = 'white';
+		$colors.hide();
+		$('#thisColorText').remove();
+		$inputLabel.attr('placeholder', 'Label it');
+	});
+
+	//delete button
+	//delete mode on or off determines if clicking on a list item will change
+	//its color or delete the item. when delete is on, a trash can icon will
+	//display on all listed items and clicking on it will animate the list
+	//item and delete it. when delete mode is off, clicking on list items will
+	//change its color
+
+//	$('#deleteMode').on('click', function() {
+//		if(deleteMode === 0) {
+//			deleteMode = 1;
+//			$('#deleteMode').removeClass('buttonOff');
+//			$('#deleteMode').addClass('buttonOn');
+//		}else {
+//			$('#deleteMode').removeClass('buttonOn');
+//			$('#deleteMode').addClass('buttonOff');
+//			deleteMode = 0;
+//		};
+//		alert($('#deleteMode').className);
+//	});
+
+	$('#deleteMode').on('click', function() {
+		$(this).toggleClass('on');
+		alert($('#deleteMode').className);
+	});
+
+
+
+	//changes color of list item or deletes it based on deleteMode on/off
+	$('ul').on('click', 'li', function() {
+
+		if(deleteMode === 0) {
+
+			var i = 0;
+			var thisColor = this.className;	
+
+			while(thisColor != colorWheel[i]) {
+				i++;
+			};
+			i++;
+			if (i > 3) {
+				i = 0;
+			};
+			$(this).attr('class', colorWheel[i]);
+		} else {
+			$(this).remove();
+		};
+	});
+
+});
